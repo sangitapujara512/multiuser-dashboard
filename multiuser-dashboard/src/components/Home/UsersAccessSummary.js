e8b907d68ac812ea526ad294929ddee846186e02
sangitapujara512 <sangitapujara512@gmail.com>
2020-07-30 07:25:11 +0530

    Added Icons
    Added Icons

--- multiuser-dashboard/src/components/Home/UsersAccessSummary.js
+++ multiuser-dashboard/src/components/Home/UsersAccessSummary.js
@@ -8,6 +8,12 @@ import ListItemText from "@material-ui/core/ListItemText";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import CommentIcon from "@material-ui/icons/Comment";
+import HomeIcon from '@material-ui/icons/Home';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import DescriptionIcon from '@material-ui/icons/Description';
+import SettingsIcon from '@material-ui/icons/Settings';
+import AssessmentIcon from '@material-ui/icons/Assessment';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -39,16 +45,93 @@ export default function UsersAccessSummary({data}) {
 
     setChecked(newChecked);
   };
-
+let iconName=[]
   return (
     <List className={classes.root}>
+   
+
+   {data && data.map((value)=>{
+       let access=Object.entries(value && value.access)
+       access.map((item)=>{
+           console.log("item[0]",item[0])
+        iconName.push(<div style={{height:'fit-content',display:'block',marginBottom:'20px'}}>{item[0]}</div>)
+       })
+   })        
+
+     }
+     <div style={{marginTop:"43px"}}>
+     <div>
+         <div style={{display:'flex'}}>
+     <HomeIcon />
+      {iconName[0]}
+      </div>
+      <div style={{display:'flex'}}>
+     <LocationOnIcon />
+     {iconName[1]}
+      </div>
+      <div style={{display:'flex'}}>
+     <LocationOnIcon />
+     {iconName[2]}
+      </div>
+      <div style={{display:'flex'}}>
+     <LocationOnIcon />
+     {iconName[3]}
+      </div>
+      <div style={{display:'flex'}}>
+     <NotificationsNoneIcon />
+     {iconName[4]}
+      </div>
+      <div style={{display:'flex'}}>
+     <NotificationsNoneIcon />
+     {iconName[5]}
+      </div>
+      
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[6]}
+      </div>
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[7]}
+      </div>
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[8]}
+      </div>
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[9]}
+      </div>
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[10]}
+      </div>
+      <div style={{display:'flex'}}>
+     <DescriptionIcon />
+     {iconName[11]}
+      </div>        
+    <div style={{display:'flex'}}>
+     <AssessmentIcon />
+     {iconName[12]}
+      </div>
+      <div style={{display:'flex'}}>
+     <AssessmentIcon />
+     {iconName[13]}
+      </div>
+      <div style={{display:'flex'}}>
+     <SettingsIcon />
+     {iconName[14]}
+      </div>
+     </div>
+     </div>
       {data && data.map(value => {
          
-          let access=Object.values(value && value.access)
-          console.log("value",access)
+          let access=Object.entries(value && value.access)
+          console.log("value",access[0][1])
         const labelId = `checkbox-list-label-${value.access}`;
 
         return (
+            
           <ListItem
             key={value.id}
             role={undefined}
@@ -59,16 +142,19 @@ export default function UsersAccessSummary({data}) {
           >
             <ListItemText id={labelId} primary={`${value.Role}`} />
             {access.map(item => (
+               
               <ListItemIcon  style={{display:'block'}}>
+              {/* {item} */}
+              {/* {item[0]} */}
                   {/* {item.key} */}
                 <Checkbox
                   edge="start"
-                   checked={item == 1}
+                   checked={item[1]}
                 //   checked={false}
                   tabIndex={-1}
                   disableRipple
                   inputProps={{ "aria-labelledby": labelId }}
-                 />
+                 style={{color:'grey'}}/>
               </ListItemIcon>
             ))}
 
